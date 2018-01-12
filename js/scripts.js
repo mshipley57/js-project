c8d9105c07ea66697251c1d13339655c05f2d43e
Mariah Shipley <mshipley57@gmail.com>
2018-01-12 10:04:14 -0800

    add first part of js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("form#jsquiz").submit(function(event) {
+    var company = $("select#company").val();
+    var versatility = $("select#versatility").val();
+    var rateys = parseInt($("input#rateys").val());
+    var server = parseInt($("input#server").val());
+    var froback = $("select#froback").val();
+  }
+}
