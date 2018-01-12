eea86d2d06a9e9d13ee0abbab31ab0a4959e1b8a
Mariah Shipley <mshipley57@gmail.com>
2018-01-12 11:37:50 -0800

    add js functions and math

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,12 @@ $(document).ready(function() {
     var server = parseInt($("input#server").val());
     var froback = $("select#froback").val();
 
-    if (company === 'bigcompany' && server < 10) {
+    if (company === 'bigcompany' && froback === 'either') {
       $('#csharp').show();
-    } else if (company === 'smallcompany' && server <10) {
+    } else if (company === 'smallcompany' || 'bigcompany' && server  < 6 && rateys < 6) {
       $('#ruby').show();
-    }
+    } else if (company === 'bigcompany' || 'smallcompany' && server > 5 && rateys < 11)
+      $('#php').show();
 
     event.preventDefault();
   });
