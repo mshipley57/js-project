b0432f03bb071b361c14b128154406e58b885843
Mariah Shipley <mshipley57@gmail.com>
2018-01-12 10:44:38 -0800

    add images for each track

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,13 @@ $(document).ready(function() {
     var rateys = parseInt($("input#rateys").val());
     var server = parseInt($("input#server").val());
     var froback = $("select#froback").val();
-  }
-}
+
+    if (company === 'bigcompany' && server < 10) {
+      $('#csharp').show();
+    } else if (company === 'smallcompany' && server <10) {
+      $('#ruby').show();
+    }
+
+    event.preventDefault();
+  });
+});
